d1fcc1d4e5dd2346eef0e79ccc980eb3ef667c40
Kushang Shah <kushangshah131@gmail.com>
2021-05-04 12:56:33 +0530

    set cookies

--- Code/jsCookies.js
+++ Code/jsCookies.js
@@ -1,3 +1,27 @@
+
+class CookieThings{
+    constructor(){
+        this.allCookieNames = [];
+    }
+    setCookie(name, value, expiryDays){
+        let dt = new Date();
+        dt.setTime(dt.setTime() + (expiryDays*24*60*60*1000));
+        let cookieStr = `${name}=${value};expires=${dt.toUTCString()};path=/`;
+        document.cookie = cookieStr;
+    }
+    showCookies(){
+        console.log(document.cookie);
+    }
+}
+
 (function(){
-    console.log(document.cookie);
+    let set = document.querySelector("#set");
+    let ct = new CookieThings();
+    set.onsubmit = function(event){
+        event.preventDefault();
+        cname = set.cookie.value;
+        cdata = set.data.value;
+        ct.setCookie(cname, cdata, Number(2));
+    }
+    ct.showCookies();
 })();
