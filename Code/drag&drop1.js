285750443be8d8a4feffa258b5b1c643739f899c
Kushang Shah <kushangshah131@gmail.com>
2021-05-04 08:59:22 +0530

    advance drag and drop

--- Code/drag&drop1.js
+++ Code/drag&drop1.js
@@ -4,11 +4,41 @@ function allowDrop(event){
 }
   
 function drag(event){
-    event.dataTransfer.setData("elementId", event.target.id);
+    event.dataTransfer.setData("pickedElementId", event.target.id);
 }
   
 function drop(event){
+    // Preventing default action
     event.preventDefault();
-    var data = event.dataTransfer.getData("elementId");
-    event.target.appendChild(document.getElementById(data));
+
+    // If targeted element is image then abort
+    if(String(event.target.localName) !== String('div')) return false;
+
+    if(!event.target.className.includes("box")) return false;
+
+    // Removing whitespaces and trimming
+    let childVals = event.target.innerHTML;
+    childVals = childVals.trim().replace(/(\r\n|\n|\r)/gm, "");
+    event.target.innerHTML = childVals;
+
+    // If already element exist then abort
+    if(event.target.hasChildNodes()) return false; 
+
+    let data = event.dataTransfer.getData("pickedElementId");
+    let elmt = document.getElementById(data);
+    event.target.appendChild(elmt);
+    return true;
 }
+
+(function(){
+    
+    let divs = document.querySelectorAll(".box");
+    let i = 0;
+    divs.forEach(div => {
+        div.ondrop = function(event){ if(!drop(event)) alert("This block contains another block. Select other block");};
+        div.ondragover = function(event){ allowDrop(event); };
+        div.setAttribute('id', 'box'+i);
+        i++;
+    });
+
+})();
